1c30e401dcb98f4602549d1dd461903012d80012
HBS_System <katherinelardin@gmail.com>
2021-09-16 22:41:48 -0400

    a

---
+++ events/onJoin.js
@@ -0,0 +1,35 @@
+const commands = require('../main.js');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v8');
+
+const token = require('../token.json');
+const botid = '596683881575612429';
+
+module.exports = {
+	name: 'guildCreate',
+	execute(guild) {
+		const rest = new REST({ version: '9' }).setToken(token);
+
+		(async () => {
+			try {
+				console.log('Command: Started refreshing local application (/) commands.');
+
+				await rest.put(
+					Routes.applicationGuildCommands(botid, guild.id), //Used to reload all commands to the server this command was run in.
+					{ body: commands },
+				); 
+				//Above block is used to reload all commands to the server the command is run in.
+
+				/*await rest.put(
+					Routes.applicationCommands(botid), 
+					{ body: commands },
+				);*/ 
+				//Above block is used to publish all commands to ALL servers.
+
+				console.log('Command: Successfully reloaded local application (/) commands.');
+			} catch (error) {
+				console.log(error);
+			}
+		})();
+	},
+};
